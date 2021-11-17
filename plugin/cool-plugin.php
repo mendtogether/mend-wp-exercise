5ff1d260ab3a04727460b6c78568e22320d3cd69
Geoff Taylor <geoff@axistaylor.com>
2021-11-17 12:54:43 -0500

    initial commit

---
+++ plugin/cool-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Cool Plugin
+ * Description: It's just a cool plugin.
+ * Author: You
+ * Version: 1.0.0
+ * Text Domain: cool-plugin
+ */
+
+// Code goes here.
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
